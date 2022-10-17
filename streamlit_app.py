79156eab87df6f3dac804c354b482188201dac0d
marcellusmontilla <25737631+marcellusmontilla@users.noreply.github.com>
2022-10-17 17:56:44 +0800

    add file uploader

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-
+st.file_uploader('Upload pdf file:')
 
 total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
 num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
