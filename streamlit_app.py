e32f2a826d9bd69ee47467a6d65232ed4d9277d4
marcellusmontilla <25737631+marcellusmontilla@users.noreply.github.com>
2022-10-18 13:09:30 +0800

    get data without headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def to_excel(df):
 
 uploaded_file = st.file_uploader('Upload pdf file:')
 if uploaded_file is not None:
-    dfs = tabula.read_pdf(uploaded_file, pages="all")
+    dfs = tabula.read_pdf(uploaded_file, pages="all", pandas_options={'header': None}))
     new_df = pd.concat(dfs)
     df_xlsx = to_excel(new_df)
     st.download_button(label='📥 Download Current Result',
